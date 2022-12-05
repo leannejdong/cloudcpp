d967b28abcdb102ed346fd5d7f9dd52ea2f72979
Leanne J Dong <jdleanne@gmail.com>
2022-12-06 02:30:20 +1000

    yet to run

--- clion/main.cpp
+++ clion/main.cpp
@@ -1,17 +1,36 @@
-
-
-#include <stdio.h>
-#include <sys/sysinfo.h>
-
-int main() {
-    struct sysinfo info;
-    if (sysinfo(&info) < 0) {
-        perror(NULL);
-    }
-    printf("The system has been up for %ld seconds\n", info.uptime);
-
-    return 0;
+#include <boost/thread.hpp>
+#include <boost/chrono.hpp>
+#include <iostream>
+void wait(int seconds)
+{
+    boost::this_thread::sleep_for(boost::chrono::seconds{seconds});
 }
+void thread()
+{
+    for (int i = 0; i < 5; ++i)
+    {
+        wait(1);
+        std::cout << i << '\n';
+    }
+}
+int main()
+{
+    boost::thread t{thread};
+    t.join();
+}
+
+// #include <stdio.h>
+// #include <sys/sysinfo.h>
+
+// int main() {
+//     struct sysinfo info;
+//     if (sysinfo(&info) < 0) {
+//         perror(NULL);
+//     }
+//     printf("The system has been up for %ld seconds\n", info.uptime);
+
+//     return 0;
+// }
 
 //  LIBRARIES
 //#include <stdio.h>
